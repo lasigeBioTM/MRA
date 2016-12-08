cca11440b08f0ac5847b6153f1df5476728b998f
LLCampos <luis1212@sapo.pt>
2016-12-08 13:08:27 +0000

    try other basicauth lib

--- mra/__init__.py
+++ mra/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_basicauth import BasicAuth
+from flask_httpauth import HTTPBasicAuth
 from tasks import make_celery
 
 
@@ -10,6 +10,14 @@ app.config.from_pyfile('../config.cfg')
 
 celery = make_celery(app)
 db = SQLAlchemy(app)
-basic_auth = BasicAuth(app)
+
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_pw(username):
+    if username == app.config['BASIC_AUTH_USERNAME']:
+        return app.config['BASIC_AUTH_PASSWORD']
+
 
 import mra.views
